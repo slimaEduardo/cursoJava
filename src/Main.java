fbdddf21deb863c7dc4ee0a29f358caa5493f78c
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-07-24 21:31:58 -0300

    Teste de envio para o github

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		System.out.print("Digite um número: ");
+		num = sc.nextInt();
+		
+		if(num >= 0) {
+			System.out.println("NÃO NEGATIVO");
+		}else
+			System.out.println("NEGATIVO");
+		
+		sc.close();
+		System.out.println("");
+	}
+
+}
